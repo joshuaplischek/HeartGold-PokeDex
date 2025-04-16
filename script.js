a5e8d47b8baac3489d17aec1e65cd7a7ac45d412
joshuaplischek <plischekjoshua@web.de>
2025-04-16 20:29:19 +0200

    Add close Pokedex

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function openPokeDex() {
     let overlay = document.getElementById('pokeDexOverlay')
     overlay.style.left = "-100%";
 }
+
+function closePokeDex() {
+    let overlay = document.getElementById('pokeDexOverlay')
+    overlay.style.left = "0";
+}
