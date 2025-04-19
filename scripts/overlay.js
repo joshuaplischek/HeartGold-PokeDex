96b746a880e42ab999d2704ca6493e845a2199b1
Joshua Plischek <plischekjoshua@web.de>
2025-04-19 18:07:22 +0200

    Fix scroll bar

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,13 +1,13 @@
 function openPokeDex() {
-    let contentRef = document.getElementById(`content`)
+    let bodytRef = document.body
     let overlay = document.getElementById('pokeDexOverlay')
     overlay.style.left = "-100%";
-    contentRef.style.display = 'Flex'
+    bodytRef.style.overflowY = 'scroll'
 }
 
 function closePokeDex() {
-    let contentRef = document.getElementById(`content`)
+    let bodytRef = document.body
     let overlay = document.getElementById('pokeDexOverlay')
     overlay.style.left = "0";
-    contentRef.style.display = 'none'
+    bodytRef.style.overflowY = 'hidden'
 }
